331646ce54b144c1f76380ff0c56691f80ddeb46
justine hilario <justinemhilario.orgi1996@gmail.com>
2024-02-03 17:57:08 +0800

    final

--- src/components/infocard/index.jsx
+++ src/components/infocard/index.jsx
@@ -1,3 +1,5 @@
+import './infocard-scss/index.js'
+
 import {
     Image,
     theme
@@ -5,224 +7,93 @@ import {
 
 import React from "react"
 
+
 export const TechnologyInfoCard = ({ title, href, image }) => {
 
     const { useToken } = theme
     const { token } = useToken()
 
+    const style = {
+        backgroundColor: token.colorBgContainer,
+        boxShadow: token.boxShadow,
+        color: token.colorTextSecondary,
+    }
+    
     return (
-
-        <div
-            style={{
-                backgroundColor: token.colorBgContainer,
-                boxShadow: token.boxShadow,
-                borderRadius: "5px",
-                color: token.colorTextSecondary,
-                lineHeight: "22px",
-                padding: "16px 19px",
-                minWidth: "220px",
-                flex: 1
-            }}
-        >
-        
-            <div
-                style={{
-                    display: "flex",
-                    gap: "8px",
-                    alignItems: "center",
-                }}
-            >
-
-                <div
-                    style={{
-                        width: 70,
-                        height: 70,
-                        lineHeight: "22px",
-                        backgroundSize: "100%",
-                        padding: "14px 16px",
-                    }}
-                >
+        <div style={style} className='infocard-technology-page'>
+            <div className='group-technology-page'>
+                <div className='technology-image'>
                     <Image src={image}/>
                 </div>
-
-                <div
-                    style={{
-                        fontSize: "18px",
-                        color: token.colorText,
-                    }}
-                >
-                    <a href={href} target="_blank" rel="noreferrer" style={{color: token.colorText,}} >
-                        {title}
-                    </a>
+                <div className='technology-title'>
+                    {title}
                 </div>
-
             </div>
-
         </div>
-
     )
-
 }
 
 export const ProjectsInfoCard = ({ title, href, image, image2, desc }) => {
 
     const { useToken } = theme
     const { token } = useToken()
-  
+    
+    const style = {
+        backgroundColor: token.colorBgContainer,
+        boxShadow: token.boxShadow,
+        color: token.colorTextSecondary,
+    }
+    
     return (
-
-        <div
-            style={{
-                backgroundColor: token.colorBgContainer,
-                boxShadow: token.boxShadow,
-                borderRadius: "8px",
-                color: token.colorTextSecondary,
-                lineHeight: "22px",
-                padding: "16px 19px",
-                minWidth: "220px",
-                flex: 1,
-            }}
-        >
-  
-            <div
-                style={{
-                    display: "flex",
-                    gap: "8px",
-                    alignItems: "center",
-                }}
-            >
-  
-                <div
-                    style={{
-                        width: 80,
-                        height: 80,
-                        lineHeight: "22px",
-                        backgroundSize: "100%",
-                        padding: "8px 16px 16px 12px",
-                    }}
-                >
+        <div style={style} className='infocard-project-page'>
+            <div className='group-project-page'>
+                <div className='project-image'>
                     <Image src={image} style={{borderRadius: 2,}}/>
                 </div>
-    
-                <div
-                    style={{
-                        fontSize: "18px",
-                        color: token.colorText,
-                        paddingBottom: 8,
-                        marginTop: 10
-                    }}
-                >
+                <div className='project-title'>
                     <a href={href} target="_blank" rel="noreferrer" style={{color: token.colorText,}} >
                         {title}
                     </a>
                 </div>
-
             </div>
-            
-            <div
-                style={{
-                    display: "flex",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "274px auto",
-                    marginBottom: 32,
-                }}
-            >
+            <div className='project-image-1'>
                 <Image src={image2} style={{borderRadius: 2,}}/>
             </div>
-
-            <div
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "14px",
-                    color: token.colorTextSecondary,
-                    textAlign: "justify",
-                    lineHeight: "22px",
-                    marginBottom: 8,
-                }}
-            >
+            <div className='project-description'>
                 {desc}
             </div>
-            
         </div>
-
     )
-
 }
   
 export const WorkInfoCard = ({ title, date, href, image }) => {
 
     const { useToken } = theme
     const { token } = useToken()
-  
-    return (
 
-        <div
-            style={{
-                backgroundColor: token.colorBgContainer,
-                boxShadow: token.boxShadow,
-                borderRadius: "8px",
-                color: token.colorTextSecondary,
-                lineHeight: "22px",
-                padding: "16px 19px",
-                minWidth: "220px",
-                flex: 1,
-            }}
-        >
-  
-            <div
-                style={{
-                    display: "flex",
-                    gap: "4px",
-                    alignItems: "center",
-                    marginTop: 10
-                }}
-            >
-  
-                <div
-                    style={{
-                        width: 80,
-                        height: 80,
-                        lineHeight: "22px",
-                        backgroundSize: "100%",
-                        padding: "8px 16px 16px 12px",
-                    }}
-                >
+    const style = {
+        backgroundColor: token.colorBgContainer,
+        boxShadow: token.boxShadow,
+        color: token.colorTextSecondary,
+    }
+    
+    return (
+        <div style={style} className='infocard-work-experience-page'>
+            <div className='group-work-experience-page'>
+                <div className='work-experience-image'>
                     <Image src={image} style={{borderRadius: 2,}}/>
                 </div>
-    
-                <div
-                    style={{
-                        fontSize: "20px",
-                        color: token.colorText,
-                        paddingBottom: 8,
-                        marginTop: 10
-                    }}
-                >
+                <div className='work-experience-title'>
                     <a href={href} target="_blank" rel="noreferrer" style={{color: token.colorText,}} >
                         {title}
                     </a>
-
-                    <div
-                        style={{
-                            fontSize: "14px",
-                            color: token.colorTextSecondary,
-                            textAlign: "justify",
-                            lineHeight: "28px",
-                            marginBottom: 8
-                        }}
-                    >
+                    <div className='work-experience-date' >
                         {date}
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
     )
-
 }
 
 
@@ -231,58 +102,24 @@ export const FooterInfoCard = ({ title, href, image }) => {
     const { useToken } = theme
     const { token } = useToken()
 
+    const style = {
+        backgroundColor: token.colorBgContainer,
+        boxShadow: token.boxShadow,
+        color: token.colorTextSecondary,
+    }
+
     return (
-
-        <div
-            style={{
-                backgroundColor: false,
-                boxShadow: token.boxShadow,
-                borderRadius: "8px",
-                color: token.colorTextSecondary,
-                minWidth: "220px",
-                flex: 1,
-            }}
-        >
-        
-            <div
-                style={{
-                    display: "flex",
-                    gap: "8px",
-                    alignItems: "center",
-                    marginTop: 10
-                }}
-            >
-
-                <div
-                    style={{
-                        width: 60,
-                        height: 60,
-                        lineHeight: "22px",
-                        backgroundSize: "100%",
-                        padding: "8px 16px 16px 12px",
-                        marginTop: 10
-                    }}
-                >
+        <div style={style} className='infocard-footer-page'>
+            <div className='group-footer-page'>
+                <div className='footer-image'>
                     <Image src={image}/>
                 </div>
-
-                <div
-                    style={{
-                        fontSize: "18px",
-                        color: token.colorText,
-                        paddingBottom: 8,
-                        marginTop: 10
-                    }}
-                >
+                <div className='footer-title'>
                     <a href={href} target="_blank" rel="noreferrer" style={{color: token.colorText,}} >
                         {title}
                     </a>
                 </div>
-                
             </div>
-
         </div>
-
     )
-
 }
